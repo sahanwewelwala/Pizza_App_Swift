767aaeff58ae08f009beacb2ac44bb0771fff4aa
Adeegithub <arunodagunawardana@gmail.com>
2023-11-10 14:31:41 +0530

    Initial Commit

---
+++ SelfPizzaApp/SelfPizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfPizzaAppApp.swift
+//  SelfPizzaApp
+//
+//  Created by Adeesha Arunoda Gunawardana on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SelfPizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
